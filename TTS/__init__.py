efc186e3764342ff5d58aa5abad01fac907b06f2
NoodleInWater <why097729@gmail.com>
2025-09-10 22:00:33 +0800

    No need to login wandb now

--- TTS/__init__.py
+++ TTS/__init__.py
@@ -1,12 +1,9 @@
 import os
 import dotenv
-import wandb
 
 dotenv.load_dotenv()
 
 with open(os.path.join(os.path.dirname(__file__), "VERSION"), "r", encoding="utf-8") as f:
     version = f.read().strip()
-
-wandb.login(key=os.getenv('WANDB_KEY'))
 
 __version__ = version
